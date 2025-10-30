f4a08e78d945f7e1093f328cea0057c43c13c3f1
radhoo-PC <radhoo-PC@DESKTOP-74H41PU>
2025-10-30 15:13:32 +0200

    Testing HeapSort

---
+++ index.js
@@ -0,0 +1,5 @@
+import heapSort from "./database.js";
+
+const Hs = heapSort([12, 11, 13, 5, 6, 7, -2, -8, 13, 1]);
+console.log("Sorted array is:", Hs);
+//  Output: Sorted array is: [ -8, -2, 1, 5, 6, 7, 11, 12, 13, 13 ]
